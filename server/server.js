73df294259a2a4a8553b0e4d855dcbeda3d5e7f3
riyadhbd2 <riyadhbd2@gmail.com>
2025-02-18 21:08:28 +0100

    initial backedn server added

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+
+// App config
+const port = process.env.PORT || 6002;
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+
+
+// API routes
+app.get('/', (req, res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is listening on port ${port}`);
+})
